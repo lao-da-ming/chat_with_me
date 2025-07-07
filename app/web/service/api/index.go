9f1e435b52f52c158ab0cf374931ba7ba031f7f7
laodaming <1059642220@qq.com>
2025-07-07 15:19:46 +0800

    1

--- app/web/service/api/index.go
+++ app/web/service/api/index.go
@@ -13,15 +13,5 @@ func NewIndexController(logger *zap.Logger) *IndexController {
 	return &IndexController{logger: logger}
 }
 func (i *IndexController) Home(c *gin.Context) {
-	/*conn, ok := ws.ConnectPool.Load(c.Param("key"))
-	if !ok {
-		c.JSON(200, gin.H{"msg": "链接不存在"})
-	}
-	fd, ok := conn.(*ws.ConnObj)
-	if !ok {
-		c.JSON(200, gin.H{"msg": "断言链接失败"})
-	}
-	fd.WriteToSendChan([]byte("9999999999999999999999999"))*/
-	i.logger.Error("hahahahah")
 	c.JSON(200, gin.H{"msg": "OK"})
 }
