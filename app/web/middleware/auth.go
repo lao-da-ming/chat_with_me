012c96eba52ad3091c49817dd2e428e62e493ed7
laodaming <1059642220@qq.com>
2025-07-08 10:40:31 +0800

    1

--- app/web/middleware/auth.go
+++ app/web/middleware/auth.go
@@ -3,17 +3,10 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"net/http"
 )
 
 func AuthMiddleware(logger *zap.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		protocol := ctx.GetHeader("Sec-WebSocket-Protocol")
-		if protocol == "" {
-			logger.Error("token为空")
-			ctx.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
 		ctx.Next()
 	}
 }
