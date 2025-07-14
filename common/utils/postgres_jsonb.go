4f9d0d49537ecfdc070311459641a4547a482f7b
laodaming <1059642220@qq.com>
2025-07-14 10:58:28 +0800

    1

--- common/utils/postgres_jsonb.go
+++ common/utils/postgres_jsonb.go
@@ -9,15 +9,15 @@ import (
 )
 
 // 设置postgres jsonb属性
-func SetPgJsonbValue(db *gorm.DB, model any, rowId int64, dbColumn string, objectPath []string, value any) error {
-	lenPath := len(objectPath)
+func SetPgJsonbValue(db *gorm.DB, model any, rowId int64, dbColumn string, path []string, value any) error {
+	lenPath := len(path)
 	//空就不需要处理
 	if lenPath == 0 {
 		return nil
 	}
 	//只有一层直接赋值
 	if lenPath == 1 {
-		missPath := joinPostgresJsonbPath(objectPath)
+		missPath := joinPostgresJsonbPath(path)
 		err := setValue(db, model, rowId, dbColumn, missPath, value)
 		if err != nil {
 			return errors.New(fmt.Sprintf("set jsonb err，path:{%s},err=%s", missPath, err.Error()))
@@ -25,7 +25,7 @@ func SetPgJsonbValue(db *gorm.DB, model any, rowId int64, dbColumn string, objec
 		return nil
 	}
 	//检查前面的路径是否对象
-	selectStr := buildGetJsonbPathTypeSelectStr(dbColumn, objectPath)
+	selectStr := buildGetJsonbPathTypeSelectStr(dbColumn, path)
 	var checkResult string
 	if err := db.Model(model).Where("id = ?", rowId).Select(selectStr + " as result").Scan(&checkResult).Error; err != nil {
 		return err
@@ -34,15 +34,15 @@ func SetPgJsonbValue(db *gorm.DB, model any, rowId int64, dbColumn string, objec
 	for index, attrType := range pathAttrTypes {
 		//最后一个，直接赋值
 		if index == lenPath-1 {
-			missObjVal := buildMissPathVal(objectPath, lenPath, value)
-			missPath := joinPostgresJsonbPath(objectPath)
+			missObjVal := buildMissPathVal(path, lenPath, value)
+			missPath := joinPostgresJsonbPath(path)
 			err := setValue(db, model, rowId, dbColumn, missPath, missObjVal)
 			if err != nil {
 				return errors.New(fmt.Sprintf("set jsonb err，path:{%s},err=%s", missPath, err.Error()))
 			}
 			return nil
 		}
-		cutPath := objectPath[:index+1]
+		cutPath := path[:index+1]
 		tipPath := strings.Join(cutPath, ".")
 		switch attrType {
 		case "string": //字符串
@@ -54,7 +54,7 @@ func SetPgJsonbValue(db *gorm.DB, model any, rowId int64, dbColumn string, objec
 		case "object": //对象
 			continue
 		case "null": //不存在字段
-			missObjVal := buildMissPathVal(objectPath, index+1, value)
+			missObjVal := buildMissPathVal(path, index+1, value)
 			missPath := joinPostgresJsonbPath(cutPath)
 			err := setValue(db, model, rowId, dbColumn, missPath, missObjVal)
 			if err != nil {
@@ -74,22 +74,22 @@ func setValue(db *gorm.DB, model any, rowId int64, dbColumn string, missPath str
 }
 
 // jsonb路径{a,b,c}
-func joinPostgresJsonbPath(objectPath []string) string {
-	return "{" + strings.Join(objectPath, ",") + "}"
+func joinPostgresJsonbPath(path []string) string {
+	return "{" + strings.Join(path, ",") + "}"
 }
 
 // 连接jsonb路径链路 targetColumn->a->b
-func joinPostgresJsonbPathChain(dbColumn string, objectPath []string) string {
-	lenPath := len(objectPath)
+func joinPostgresJsonbPathChain(dbColumn string, path []string) string {
+	lenPath := len(path)
 	if lenPath == 0 {
 		return ""
 	}
 	if lenPath == 1 {
-		return dbColumn + "->'" + objectPath[0] + "'"
+		return dbColumn + "->'" + path[0] + "'"
 	}
 	pathBuilder := strings.Builder{}
 	pathBuilder.WriteString(dbColumn)
-	for _, v := range objectPath {
+	for _, v := range path {
 		pathBuilder.WriteString("->")
 		pathBuilder.WriteString("'")
 		pathBuilder.WriteString(v)
@@ -99,23 +99,23 @@ func joinPostgresJsonbPathChain(dbColumn string, objectPath []string) string {
 }
 
 // 构建自缺失的中间对象
-func buildMissPathVal(objectPath []string, beginIndex int, value any) any {
-	if beginIndex >= len(objectPath) {
+func buildMissPathVal(path []string, beginIndex int, value any) any {
+	if beginIndex >= len(path) {
 		return value
 	}
 	return map[string]any{
-		objectPath[beginIndex]: buildMissPathVal(objectPath, beginIndex+1, value),
+		path[beginIndex]: buildMissPathVal(path, beginIndex+1, value),
 	}
 }
 
 // 构建检查路径用的select的值
-func buildGetJsonbPathTypeSelectStr(dbColumn string, objectPath []string) string {
-	lenPath := len(objectPath)
+func buildGetJsonbPathTypeSelectStr(dbColumn string, path []string) string {
+	lenPath := len(path)
 	//检查前面的路径是否对象
 	selectStr := strings.Builder{}
 	for i := 0; i < lenPath; i++ {
 		selectStr.WriteString("COALESCE(jsonb_typeof(")
-		selectStr.WriteString(joinPostgresJsonbPathChain(dbColumn, objectPath[:i+1]))
+		selectStr.WriteString(joinPostgresJsonbPathChain(dbColumn, path[:i+1]))
 		selectStr.WriteString(")::TEXT,'null')")
 		if i < lenPath-1 {
 			selectStr.WriteString(" || ',' || ")
